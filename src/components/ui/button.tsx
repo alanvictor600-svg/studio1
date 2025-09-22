6d501a0ad6579acbf091b5390abf1fc287242475
allan victor <alan.victor600@gmail.com>
2025-09-22 00:25:28 +0000

    aqui em modo claro tem que ser resposivel e se adquar a cor para poder c

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -19,7 +19,7 @@ const ButtonVariants = cva(
           "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
-        ghost: "hover:bg-accent text-accent-foreground hover:text-accent-foreground",
+        ghost: "text-foreground hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
